357fc989449a9ebb9aca4504c7c856f6b9208eec
Josh <josh.e.fielding@gmail.com>
2019-05-14 19:28:20 +1000

    notes

--- unit-2-list-pagination-and-filtering/javascript-and-the-dom/making-changes-to-the-dom/practice-manipulating-the-dom.js
+++ unit-2-list-pagination-and-filtering/javascript-and-the-dom/making-changes-to-the-dom/practice-manipulating-the-dom.js
@@ -7,23 +7,40 @@ h1.style.color = "pink";
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
-
 document.querySelector('.desc').innerHTML = 'Text goes here!'; 
 
 // 4: Set the class of the <ul> to 'list'
-document.getElementsByTagName("ul")[0].className = "list";
+document.getElementsByTagName('ul')[0].className = 'list';
 
 // 5: Create a new list item and add it to the <ul>
 var newListItem = document.createElement('li');
-ul.appendChild(li); 
+let ul = document.querySelector('ul'); 
+ul.appendChild(newListItem); 
 
 // 6: Change all <input> elements from text fields to checkboxes
-document.querySelector('input').innerHTML 
+// document.querySelector('input').type = 'checkbox'; 
+
+const i = document.getElementsByTagName('li').type = 'checkbox'; 
 
 
-type="checkbox"
 // 7: Create a <button> element, and set its text to 'Delete'
 // Add the <button> inside the '.extra' <div>
+const btn = document.createElement('button'); 
+btn.innerHTML = 'Delete me';
+document.querySelector('.extra').appendChild(btn); 
 
 
 // 8: Remove the '.extra' <div> element from the DOM when a user clicks the 'Delete' button
+const removeItemButton = document.querySelector('button'); 
+
+removeItemButton.addEventListener('click', () => {
+
+  const removeBtn = document.querySelector('button'); 
+  
+  const extraDiv = document.querySelector('.extra p'); 
+
+  removeBtn.removeChild(extraDiv); 
+
+  // extraDiv.removeChild(removeBtn); 
+});
+
