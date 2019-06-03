65e6025819ee4c72e32d9c699da85240f6621984
Josh <josh.e.fielding@gmail.com>
2019-06-03 20:06:39 +1000

    notes

--- unit-3-interactive-form/jQuery Basics/introducing-jQuery/1_blog_previewer/video4/js/app.js
+++ unit-3-interactive-form/jQuery Basics/introducing-jQuery/1_blog_previewer/video4/js/app.js
@@ -3,11 +3,32 @@ $('#flashMessage').hide();
 $('#flashMessage').slideDown(1000);
 // give user 3 seconds to read the message, then slide out of view
 $('#flashMessage').delay(3000);
-$('#flashMessage').slideUp(); 
+$('#flashMessage').slideUp();
 
-// can chain together methods in jQuery on the same element like so
-$('#flashMessage').hide().slideDown(1000).delay(3000).slideUp(); 
+$('#element').text();
+
+// change html within the element
+$('#element').html(); // getter because it gets the value
+$('#element').html("<p>I am setting this HTML!</p>"); // setter because it sets the value
+
+const title = "My First Blog Post";
+const content = "This is my <strong>first</strong> post!";
+
+// returns the contents of $('.product-description') as plain text.
+$('.product-description').text(); 
 
 
+$('#flashMessage').hide();
 
+$('#previewButton').click(function () {
+  // do something here
+  // .val grabs the user's input / retrieves information from a form input field
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
 
+  $('#blogTitlePreview').text(title);
+  $('#blogContentPreview').html(content);
+  // $('#blogContentPreview').text(content); // also works, but won't display the <strong></strong> tags correctly like the .html method
+  // can chain together methods in jQuery on the same element like so
+  $('#flashMessage').slideDown(1000).delay(3000).slideUp();
+});
