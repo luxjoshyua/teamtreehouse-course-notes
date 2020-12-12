1289f54cb566cace2434dbf8c17aeab870ae3c98
luxjoshyua <josh.e.fielding@gmail.com>
2020-12-12 13:31:04 +1100

    added unit 8 notes

--- unit-8-sql-library-manager/sequelize-orm-with-express/models/article.js
+++ unit-8-sql-library-manager/sequelize-orm-with-express/models/article.js
@@ -1,12 +1,42 @@
 "use strict";
 const Sequelize = require("sequelize");
+// import moment js library
+const moment = require("moment");
 
 module.exports = (sequelize) => {
-  class Article extends Sequelize.Model {}
+  class Article extends Sequelize.Model {
+    // create method
+    publishedAt() {
+      // store the formatted date, set to moment method
+      const date = moment(this.createdAt).format("MMMM D, YYYY, h:mma");
+      // holds the formatted date
+      return date;
+    }
+
+    // generate short description
+    shortDescription() {
+      // if the body length is greater than 200 characters,
+      // return character string starting at 0 and ending at 200 with an ellipsis to indicate article is truncated,
+      // otherwise return the body desc in full
+      const shortDesc =
+        this.body.length > 200
+          ? this.body.substring(0, 200) + "..."
+          : this.body;
+      return shortDesc;
+    }
+  }
 
   Article.init(
     {
-      title: Sequelize.STRING,
+      title: {
+        type: Sequelize.STRING,
+        validate: {
+          notEmpty: {
+            msg: '"Title" is required',
+          },
+        },
+      },
+
       author: Sequelize.STRING,
       body: Sequelize.TEXT,
     },
--- unit-8-sql-library-manager/sequelize-orm-with-express/routes/articles.js
+++ unit-8-sql-library-manager/sequelize-orm-with-express/routes/articles.js
@@ -41,12 +41,28 @@ router.get("/new", (req, res) => {
 router.post(
   "/",
   asyncHandler(async (req, res) => {
-    // req.body maps to the form inputs
-    // console.log(req.body);
-    // pass create the request body
-    const article = await Article.create(req.body);
-    // redirects to the URL path for the individual article id e.g. localhost:3000/articles/1
-    res.redirect("/articles/" + article.id);
+    let article;
+    try {
+      // req.body maps to the form inputs
+      // console.log(req.body);
+      // pass create the request body
+      article = await Article.create(req.body);
+      // redirects to the URL path for the individual article id e.g. localhost:3000/articles/1
+      res.redirect("/articles/" + article.id);
+    } catch (error) {
+      // checking the error
+      if (error.name === "SequelizeValidationError") {
+        article = await Article.build(req.body);
+        res.render("articles/new", {
+          article,
+          errors: error.errors,
+          title: "New Article",
+        });
+      } else {
+        // error caught in the asyncHandler's catch block
+        throw Error;
+      }
+    }
   })
 );
 
@@ -54,7 +70,16 @@ router.post(
 router.get(
   "/:id/edit",
   asyncHandler(async (req, res) => {
-    res.render("articles/edit", { article: {}, title: "Edit Article" });
+    // find the article by using the requested parameter router id
+    const article = await Article.findByPk(req.params.id);
+    if (article) {
+      res.render("articles/edit", {
+        article: article,
+        title: "Edit Article",
+      });
+    } else {
+      res.sendStatus(404);
+    }
   })
 );
 
@@ -65,7 +90,12 @@ router.get(
     // find article by primary key
     // params returns parameters in the matched route
     const article = await Article.findByPk(req.params.id);
-    res.render("articles/show", { article: article, title: article.title });
+    if (article) {
+      res.render("articles/show", { article: article, title: article.title });
+    } else {
+      // send a 404 status to the client
+      res.sendStatus(404);
+    }
   })
 );
 
@@ -73,7 +103,32 @@ router.get(
 router.post(
   "/:id/edit",
   asyncHandler(async (req, res) => {
-    res.redirect("/articles/");
+    let article;
+    try {
+      // initialise article varialble that awaits the search result
+      article = await Article.findByPk(req.params.id);
+      if (article) {
+        // await is async, so it awaits the promise, then once the promise is fulfilled, do something
+        // pass the request body once the promise is fulfilled
+        await article.update(req.body);
+        res.redirect("/articles/" + article.id);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        article = await Article.build(req.body);
+        // make sure correct article gets updated
+        article.id = req.params.id;
+        res.render("articles/edit", {
+          article,
+          errors: error.errors,
+          title: "Edit Article",
+        });
+      } else {
+        throw error;
+      }
+    }
   })
 );
 
@@ -81,7 +136,17 @@ router.post(
 router.get(
   "/:id/delete",
   asyncHandler(async (req, res) => {
-    res.render("articles/delete", { article: {}, title: "Delete Article" });
+    // find the article to delete by using primary key or id
+    const article = await Article.findByPk(req.params.id);
+    if (article) {
+      // once the article to delete is retrieved, pass the article object
+      res.render("articles/delete", {
+        article: article,
+        title: "Delete Article",
+      });
+    } else {
+      res.sendStatus(404);
+    }
   })
 );
 
@@ -89,7 +154,16 @@ router.get(
 router.post(
   "/:id/delete",
   asyncHandler(async (req, res) => {
-    res.redirect("/articles");
+    // find the article to destroy
+    const article = await Article.findByPk(req.params.id);
+    if (article) {
+      // then destroy it using destroy() method
+      // note destroy() is an asynchronous handler, so need to wait for the promise to be fulfilled
+      await article.destroy();
+      res.redirect("/articles");
+    } else {
+      res.sendStatus(404);
+    }
   })
 );
 
